660622f409c7a350cf865d240395962e61a0b6a0
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2023-12-27 21:40:32 +0200

    create Reverse that reverses a string and prints the middle char

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,14 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String value = args[0];
+		int middle = ((value.length() % 2) == 0) ? ((value.length() / 2) - 1) : (int) (double) (value.length() / 2);
+		String reversed ="";
+		for(int i = value.length()-1; i>= 0 ; i--){
+			System.out.print(value.charAt(i));
+			reversed += value.charAt(i);
+		}
+		System.out.println();
+		System.out.print("The middle character is: " + reversed.charAt(middle));
 	}
 }
