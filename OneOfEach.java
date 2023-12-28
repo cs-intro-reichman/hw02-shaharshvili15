d9690b7617d02af345bf7eb29c6d793f03d5bd56
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2023-12-28 10:15:33 +0200

    write OneOfEach that will check if a couples have at 1 boy and 1 girl

--- OneOfEach.java
+++ OneOfEach.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 /**
  *  Simulates the formation of a family in which the parents decide 
@@ -5,6 +6,27 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		Random rand = new Random();
+		//0 is boy 1 is girl
+		int randomNumber = rand.nextInt(2);
+		char boyOrGirl = randomNumber == 0 ? 'b' : 'g';
+		System.out.print(boyOrGirl + " ");
+		boolean boy = randomNumber == 0;
+		boolean girl = randomNumber == 1;
+		int childrenCount=1;
+		while (boy == false || girl == false){
+			childrenCount++;
+			randomNumber = rand.nextInt(2);
+			if(randomNumber == 0){
+				System.out.print("b ");
+				boy = true;
+			}
+			else{
+				System.out.print("g ");
+				girl = true;
+			}
+		}
+		System.out.println();
+		System.out.printf("You made it... and you now have %d children" , childrenCount);
 	}
 }
