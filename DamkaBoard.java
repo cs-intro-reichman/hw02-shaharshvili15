3b1ff351036f6a05d6302ac0a546cbc43bc16430
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2023-12-28 09:55:22 +0200

    write DamkaBoard , gets a number and prints a damka board in the size of the number

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,21 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int numberOrLinesAndColumns = Integer.parseInt(args[0]);
+		for (int line= 1; line<=numberOrLinesAndColumns ; line++){
+			String currentLine ="";
+			for(int column = 1;column<=numberOrLinesAndColumns;column++ ){
+				if(line%2 == 0 && column == 1){
+					currentLine += " *";
+				}
+				else if (line%2 != 0 && column == 1) {
+					currentLine += "*";
+				}
+				else{
+					currentLine +=" *";
+				}
+			}
+			System.out.println(currentLine);
+		}
 	}
 }
