41f2e27b559d8d831628d447b9c9cca6fbe02fd8
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2024-01-03 11:45:16 +0200

    fix reversed

--- Reverse.java
+++ Reverse.java
@@ -5,7 +5,7 @@
 public class Reverse {
 	public static void main (String[] args){
 		String value = args[0];
-		int middle = ((value.length() % 2) == 0) ? ((value.length() / 2))-1 : (int) (double) (value.length() / 2);
+		int middle = ((value.length() % 2) == 0) ? ((value.length() / 2)) : (int) (double) (value.length() / 2);
 		String reversed ="";
 		for(int i = value.length()-1; i>= 0 ; i--){
 			System.out.print(value.charAt(i));
